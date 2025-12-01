32c42c6fbf77450b90fe29b4c72a99a25bbfcf0f
Aruna Meiyeppen <aruna.meiyeppen@digitalistgroup.com>
2025-12-01 19:43:25 +0200

    added hashing to filenames as part of cache busting

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import eslint from '@nabla/vite-plugin-eslint';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), eslint()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Customize entry point filenames
+        entryFileNames: `[name].[hash].js`,
+        // Customize chunk filenames (for code-split modules)
+        chunkFileNames: `[name].[hash].js`,
+        // Customize asset filenames (images, CSS, fonts, etc.)
+        assetFileNames: `assets/[name].[hash][extname]`,
+      },
+    },
+  },
 });
